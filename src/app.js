3e9b78d8d26a61c62e5505af48f7852e8b5b9fa8
carmencami <carmengreta3@outlook.es>
2022-07-06 19:36:46 +0000

    dominio

--- src/app.js
+++ src/app.js
@@ -7,5 +7,17 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let extension = [".net", ".com", ".es", ".us", ".io"];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let x = 0; x < adj.length; x++) {
+      for (let j = 0; j < noun.length; j++) {
+        for (let k = 0; k < extension.length; k++) {
+          console.log(pronoun[i] + adj[x] + noun[j] + extension[k]);
+        }
+      }
+    }
+  }
 };
